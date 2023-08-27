d0c875bf4165968c20cb3fbedcd6e92a328f7380
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-26 18:09:49 -0600

    shifted the home page header down and added space between Los Angeles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // useful variables
-const firstDivHistoryAnchor = document.getElementById("history-link-first");
-const firstDivTeamAnchor = document.getElementById("team-link-first");
+const homePageHistoryAnchor = document.getElementById("history-link-first");
+const homePageTeamAnchor = document.getElementById("team-link-first");
 
 // set the appropriate navigation for each link anchor
 addEventListener("click", (e) => {
-  if (e.target.id === firstDivHistoryAnchor.id) {
-    firstDivHistoryAnchor.href = "#second-container";
+  if (e.target.id === homePageHistoryAnchor.id) {
+    homePageHistoryAnchor.href = "#second-container";
   } else {
-    firstDivTeamAnchor.href = "#third-container";
+    homePageTeamAnchor.href = "#third-container";
   }
 });
