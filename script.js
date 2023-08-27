4ae93574a7b6bb39ae79ffdd1bd82024412d6a6c
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-27 16:44:27 -0600

    current progress. almost done with 3rd section.

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 const homePageHistoryAnchor = document.getElementById("history-link-first");
 const homePageTeamAnchor = document.getElementById("team-link-first");
 
-// set the appropriate navigation for each link anchor
+const mountain1Button = document.getElementById("mountain-1-btn");
+const mountain2Button = document.getElementById("mountain-2-btn");
+
+// set the appropriate navigation for each link anchor on home page (topmost)
 addEventListener("click", (e) => {
-  if (e.target.id === homePageHistoryAnchor.id) {
+  if (e.target === homePageHistoryAnchor) {
     homePageHistoryAnchor.href = "#second-container";
-  } else {
+  }
+  if (e.target === homePageTeamAnchor) {
     homePageTeamAnchor.href = "#third-container";
+  }
+});
+
+let activeButton = "Mountain 1";
+addEventListener("click", (e) => {
+  
+  if (e.target === mountain1Button && activeButton !== "Mountain 1") {
+    activeButton = "Mountain 1";
+    mountain1Button.style.color = "rgb(187, 187, 192)";
+    mountain1Button.style.textDecoration = "underline";
+  }
+  if (e.target === mountain2Button && activeButton !== "Mountain 2") {
+    activeButton = "Mountain 2";
+    mountain2Button.style.color = "rgb(187, 187, 192)";
+    mountain2Button.style.textDecoration = "underline";
   }
 });
