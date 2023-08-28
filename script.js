2894f29d4d91b920b8cf8ee19bcfd127b5382bbd
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-28 15:47:17 -0600

    going to finish up 3rd container today so that means adding the html for when both buttons is pressed.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // useful variables
 const homePageHistoryAnchor = document.getElementById("history-link-first");
 const homePageTeamAnchor = document.getElementById("team-link-first");
+const middlePageHistoryAnchor = document.getElementById("history-link-second");
+const middlePageTeamAnchor = document.getElementById("team-link-second");
+
+const footerPageContainer = document.getElementById("third-container");
 
 const mountainButtonsContainer = document.querySelector(
   ".mountain-buttons-container"
@@ -8,13 +12,16 @@ const mountainButtonsContainer = document.querySelector(
 const mountain1Button = document.getElementById("mountain-1-btn");
 const mountain2Button = document.getElementById("mountain-2-btn");
 
-// set the appropriate navigation for each link anchor on home page (topmost)
+// set the appropriate navigation for each link anchor on home page and middle page
 addEventListener("click", (e) => {
-  if (e.target === homePageHistoryAnchor) {
-    homePageHistoryAnchor.href = "#second-container";
+  if (
+    e.target === homePageHistoryAnchor ||
+    e.target === middlePageHistoryAnchor
+  ) {
+    e.target.href = "#second-container";
   }
-  if (e.target === homePageTeamAnchor) {
-    homePageTeamAnchor.href = "#third-container";
+  if (e.target === homePageTeamAnchor || e.target === middlePageTeamAnchor) {
+    e.target.href = "#third-container";
   }
 });
 
@@ -26,11 +33,13 @@ mountainButtonsContainer.addEventListener("click", (e) => {
     activeButton = "Mountain 1";
     mountain1Button.style.color = "rgb(187, 187, 192)";
     mountain1Button.style.textDecoration = "underline";
+    displayMountain1Page();
   }
   if (e.target === mountain2Button && activeButton !== "Mountain 2") {
     activeButton = "Mountain 2";
     mountain2Button.style.color = "rgb(187, 187, 192)";
     mountain2Button.style.textDecoration = "underline";
+    displayMountain2Page();
   }
 });
 
@@ -43,3 +52,7 @@ const resetPrevActive = (prevActiveButton) => {
     mountain2Button.style.textDecoration = "none";
   }
 };
+
+const displayMountain1Page = () => {};
+
+const displayMountain2Page = () => {};
